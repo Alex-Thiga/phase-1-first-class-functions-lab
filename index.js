117d98bc0ffaa15ee2c2b781a50d3ba19de561c9
Alex Thiga <loydeflacko@gmail.com>
2022-06-10 15:14:45 +0300

    commit tree 0bc4cc746ac2d451470ca8601365f07ec3f193ef

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = drivers => drivers.slice( 0, 2 );
+const returnLastTwoDrivers = drivers => drivers.slice( -2 );
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
+// console.log(selectingDrivers[0](drivers))
+const createFareMultiplier  =  function findMultiply ( fare )
+{
+    return function fareMultiplier( number ){
+        return fare * number
+        }
+}
+
+// fare doubler
+const fareDoubler = createFareMultiplier( 2 );
+const fareTripler = createFareMultiplier( 3 )
+
+const selectDifferentDrivers = function ( drivers, returnFirstTwoDrivers )
+{
+    return returnFirstTwoDrivers(drivers)
+}
